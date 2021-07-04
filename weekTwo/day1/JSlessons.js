45fb87485090a1b72083cae1c2558b86529049a9
Nety97 <netanielw@gmail.com>
2021-07-04 12:56:03 +0300

    Exercise

---
+++ weekTwo/day1/JSlessons.js
@@ -0,0 +1,42 @@
+// alert('hola')
+
+x = 4
+console.log('alo ma nishma')
+y = 100
+let username = true;
+let data = "Ne";
+let dataplus = data + "ty";
+console.log(dataplus)
+console.log(`si sumamos ${x} + ${y} el resultado sera ${x+y}`)
+
+
+let addressNumber = 8;
+let addressStreet = "Rotshild";
+let country = "Israel, Tel aviv";
+
+let global_address = `I study in ${addressStreet} ${addressNumber} in ${country}`;
+console.log(global_address)
+
+let birthYear = 1997;
+let futureYear = 2035;
+console.log("in " + futureYear + " i would have " +(futureYear - birthYear) + " years :)")
+console.log(`in ${futureYear} i would have ${futureYear - birthYear} years :)`)
+
+let pets = ["dog", "cat", "chicken", "cow", "fish","rabbit"];
+
+console.log(pets[0])
+// pets.pop()
+pets.splice(5,1, "horse" )
+// pets.push("horse")
+console.log(pets.length)
+console.log(pets)
+pets.splice(5, 0, "goats")
+console.log(pets);
+
+alert("Good morning sir");
+let userName= prompt("What are you Username?")
+let cookies= confirm("Do you accept the cookies?")
+
+b= "net"
+c = "y"
+console.log(b-c)
