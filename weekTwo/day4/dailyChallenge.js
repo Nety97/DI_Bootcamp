4c01e8d462d42abad79a9f7d0977d6e327301a08
Nety97 <netanielw@gmail.com>
2021-07-08 13:37:10 +0300

    mini project

--- weekTwo/day4/dailyChallenge.js
+++ weekTwo/day4/dailyChallenge.js
@@ -1,10 +1,35 @@
-function stars() {
-    let sentence = prompt("give me a sentence")
-    let newSentence = sentence.split(' ')
-    console.log(newSentence)
-    let star = "*";
-    // for (let iterator of newSentence) {
-        console.log(star * newSentence[0].length)
-    // }
+// function stars() {
+//     let sentence = prompt("give me a sentence")
+//     let newSentence = sentence.split(' ')
+//     console.log(newSentence)
+//     let star = "*";
+//     // for (let iterator of newSentence) {
+//         console.log(star * newSentence[0].length)
+//     // }
+// }
+// stars();
+
+let sentence = prompt("give me a sentence")
+let newSentence = sentence.split(' ')
+console.log(newSentence)
+let max = 0;
+// let biggestWord = newSentence;
+for (let key = 0; key < newSentence.length; key++) {
+    currentWord = newSentence[key]
+    if (currentWord.length > max) {
+        max = currentWord.length 
+        console.log(max)
+        }    
 }
-stars();
+
+let lineLenght = max +4;
+let toShow = "";
+for (let y = 1; y <= lineLenght; y++) {
+    toShow +="*" 
+}
+console.log(toShow);
+
+for (let key = 0; key < newSentence.length; key++) {
+    let word = newSentence[key];
+    let toShow = `* ${word}`
+}
