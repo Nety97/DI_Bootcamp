2691f1743cd3a840ffbcd4aaf9e9fe45af6d3373
Nety97 <netanielw@gmail.com>
2021-07-19 18:52:46 +0300

    Exercise

--- weekFour/day2/exerciseXP.js
+++ weekFour/day2/exerciseXP.js
@@ -22,5 +22,7 @@ add5(12) // result = 17
 const compose = (f, g) => (a) => f(g(a));
 const add1 = (num) => num + 1;
 const add5 = (num) => num + 5;
-compose(add1, add5)(10)
+compose(add1, add5)(10) //result = 16
+
+
 
