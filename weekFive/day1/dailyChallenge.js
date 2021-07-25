33494dafb1c026149905144f3a96f03eb279574f
Nety97 <netanielw@gmail.com>
2021-07-25 19:36:17 +0300

    DailyCha

--- weekFive/day1/dailyChallenge.js
+++ weekFive/day1/dailyChallenge.js
@@ -1,11 +1,18 @@
 
 
-    const urlSearchParams = new URLSearchParams(window.location.search);
-    const params = Object.fromEntries(urlSearchParams.entries());
-    let val = JSON.stringify(params,null ,2 )
-    console.log(val)
-    
-    
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries());
+let val = JSON.stringify(params,null ,2 )
+console.log(val)
+
+document.write(val)
+
+
+
+
+
+
+
 
 
 
