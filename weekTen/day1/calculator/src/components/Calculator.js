23b2f326eefb784fc7a23ef2300112c45d5dce14
Nety <netanielw@gmail.com>
2021-08-30 09:57:35 +0300

    calculator

--- weekTen/day1/calculator/src/components/Calculator.js
+++ weekTen/day1/calculator/src/components/Calculator.js
@@ -47,21 +47,32 @@ class Calculator extends React.Component{
                 <div>Calculator</div>
                 <h3>{this.state.firstValue} {this.state.signal} {this.state.val}</h3>
                 <h3>{this.state.finalResult}</h3>
-                <div  value='0' onClick={()=>this.calc('0')}>0</div>
-                <div  value='1' onClick={()=>this.calc('1')}>1</div>
-                <div  value='2' onClick={()=>this.calc('2')}>2</div>
-                <div  value='3' onClick={()=>this.calc('3')}>3</div>
-                <div  value='4' onClick={()=>this.calc('4')}>4</div>
-                <div  value='5' onClick={()=>this.calc('5')}>5</div>
-                <div  value='6' onClick={()=>this.calc('6')}>6</div>
-                <div  value='7' onClick={()=>this.calc('7')}>7</div>
-                <div  value='8' onClick={()=>this.calc('8')}>8</div>
-                <div  value='9' onClick={()=>this.calc('9')}>9</div>
-                <button onClick={()=>this.operation('plus')}>+</button>
-                <button onClick={()=>this.operation('minus')}>-</button>
-                <button onClick={()=>this.operation('multiply')}>*</button>
-                <button onClick={()=>this.operation('divided')}>÷</button>
-                <button onClick={this.result}>=</button>
+                <div>
+                    <div  value='0' onClick={()=>this.calc('0')}>0</div>
+                    <div  value='1' onClick={()=>this.calc('1')}>1</div>
+                    <div  value='2' onClick={()=>this.calc('2')}>2</div>
+                    <div  value='3' onClick={()=>this.calc('3')}>3</div>
+                </div>
+                <div>
+                    <div  value='4' onClick={()=>this.calc('4')}>4</div>
+                    <div  value='5' onClick={()=>this.calc('5')}>5</div>
+                    <div  value='6' onClick={()=>this.calc('6')}>6</div>
+                    <div  value='7' onClick={()=>this.calc('7')}>7</div>
+                </div>
+                <div>
+                    <div  value='8' onClick={()=>this.calc('8')}>8</div>
+                    <div  value='9' onClick={()=>this.calc('9')}>9</div>
+                    <div  value='.' onClick={()=>this.calc('.')}>.</div>
+                </div>
+                <div>
+                    <button onClick={()=>this.operation('plus')}>+</button>
+                    <button onClick={()=>this.operation('minus')}>-</button>
+                    <button onClick={()=>this.operation('multiply')}>*</button>
+                    <button onClick={()=>this.operation('divided')}>÷</button>
+                    <button onClick={this.result}>=</button>
+                </div>
+                
+               
             </div>
         )
     }
---
+++ weekTen/day1/wheather-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>API: 7BgUzcYA0l1nbufzmZGpxVD5C53miKCC</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
